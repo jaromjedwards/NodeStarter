0f8f2959e7651f398bdaa866404b6e4c8cb10aaf
Jarom Edwards <jaromjedwards@gmail.com>
2024-01-10 09:05:06 -0700

    lesson 1

---
+++ server.js
@@ -0,0 +1,14 @@
+// express web server
+var express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Jarom Edwards');
+})
+
+
+
+const port = 3000;
+
+app.listen(process.env.port || port);
+console.log('Web Server is listening at port ' + (process.env.port || port));
